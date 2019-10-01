f787cdf7fa170a2f0ae4a7ca9f09b7423f03863d
macbook pro <macbookpro@mwc-012012.dhcp.missouri.edu>
2019-10-01 18:20:42 -0500

    working maybe

--- again.py
+++ again.py
@@ -19,14 +19,14 @@ epochs = 1
 eta = 0.1# learning rate
 
 x = np.array([
-    [0, 0, 0, 1.0],  # data point (x,y,z, bias) 
-    [0, 0, 1, 1.0],  
-    [0, 1, 0, 1.0],  
-    [0, 1, 1, 1.0],
-    [1, 0, 0, 1.0],   
-    [1, 0, 1, 1.0],  
-    [1, 1, 0, 1.0],  
-    [1, 1, 1, 1.0],
+    [0, 0, 0, 1],  # data point (x,y,z, bias) 
+    [0, 0, 1, 1],  
+    [0, 1, 0, 1],  
+    [0, 1, 1, 1],
+    [1, 0, 0, 1],   
+    [1, 0, 1, 1],  
+    [1, 1, 0, 1],  
+    [1, 1, 1, 1],
 ]) 
 
 # labels
@@ -62,38 +62,31 @@ for e in range(epochs):
     for i in range(8):
         # layer 1
         v1 = np.dot(x[i, :], np.transpose(w1))
-        y11D = tanh(v1)
-        y11D = np.append(y11D, 1)
+        y1 = tanh(v1)
         # layer 2
-
-        v2 = np.dot(y11D, np.transpose(w2))
+        v2 = np.dot(np.append(y1,1), np.transpose(w2))
         y2 = tanh(v2)
-        v2 = np.append(v2, 1)
-
         #layer 3
-        y3 = np.dot(y2, np.transpose(w3))
+        y3 = np.dot(np.append(y2,1), np.transpose(w3))
 
         #backprop 
-        err = np.array(y[i, :]-y3)
+        err = -np.array(y[i, :]-y3)
         y2 = np.array([y2])
-        # e/dw3
-        dEdW3 = np.dot(np.transpose(np.array([err])),y2)
+        dEdW3 = np.dot(np.transpose(np.array([err])),np.array([np.append(y2,1)])) # e/dw3
 
 
-        y1 = np.array([y11D])
+        y1 = np.array([y1])
         errw3 = np.array(np.dot(err, w3))
-        tanhv2errw3 = errw3 * np.array([tanh(v2, True)])
-        # e/dw2
-        dEdW2 = np.dot(np.transpose(tanhv2errw3), y1)
+        tanhv2errw3 = errw3 * np.append(tanh(v2, True),1)
+        dEdW2 = np.dot(np.transpose(np.array([tanhv2errw3[0:4]])), np.array([np.append(y1,1)])) # e/dw2
 
 
 
-        
-        tanhv2errw3w2 = np.dot(tanhv2errw3, w2)
-        tanhv2errw3w2tanhv1 = tanhv2errw3w2 * np.array([tanh(v1, True)])
-        dEdW1 = np.dot(np.transpose(tanhv2errw3w2tanhv1), np.array([x[i, :]]))
+        tanhv2errw3w2 = np.dot(tanhv2errw3[0:4], w2)
+        tanhv2errw3w2tanhv1 = tanhv2errw3w2 * np.append(tanh(v1, True), 1)
+        dEdW1 = np.dot(np.transpose(np.array([tanhv2errw3w2tanhv1[0:3]])), np.array([x[i, :]]))# e/dw1
 
-
+        # adjustments
         w3 = w3 - eta*dEdW3
         w2 = w2 - eta*dEdW2
         w1 = w1 - eta*dEdW1
