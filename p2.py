4970ba94d2b144dd721166285ff08c0403ee8eaf
macbook pro <macbookpro@mwc-014193.dhcp.missouri.edu>
2019-10-09 21:15:33 -0500

    moneymaybe

--- p2.py
+++ p2.py
@@ -12,12 +12,12 @@ with open('train-images-idx3-ubyte','rb') as f:
     data = data.reshape((size, nrows, ncols))
 
 import matplotlib.pyplot as plt
-x = np.zeros(shape=(100,196))
+x = np.ones(shape=(100,197))
 for dex in range(100):
 
     im = data[dex,:,:]
     res = cv2.resize(im, dsize=(14, 14), interpolation=cv2.INTER_CUBIC)
-    x[dex] = np.array(res).flatten()
+    x[dex][0:196] = np.array(res).flatten()
 
 
 with open('train-labels-idx1-ubyte', 'rb') as f:
@@ -29,10 +29,10 @@ for i in range(10):
     for j in range(10):
         if i == j:
             labs[i][j] = 1
-Y = np.zeros(shape=(60000,10))
+y = np.zeros(shape=(60000,10))
 
 for i in range(100):
-    Y[i] = labs[data[i]]
+    y[i] = labs[data[i]]
 
 
 
@@ -40,19 +40,24 @@ def eee(val):
     return np.exp(val)
 
 
-def tanh(x, derive=False): # x is the input, derive is do derivative or not
+def tanh2(x, derive=False): # x is the input, derive is do derivative or not
     if derive:
         return (1.0 - x**2)
                            # depends on how you call the function
     return ((eee(x)-eee(-x))/(eee(x)+eee(-x)))
 
+def tanh(x, derive=False): # x is the input, derive is do derivative or not
+    if derive: # ok, says calc the deriv?
+        return x * (1.0 - x) # note, you might be thinking ( sigmoid(x) * (1 - sigmoid(x)) )
+                           # depends on how you call the function
+    return ( 1.0 / (1.0 + eee(-x)) )
 
 
-epochs = 1000
-eta = 0.1# learning rate
+epochs = 2500
+eta = 0.01# learning rate
 
 
-w1 = np.random.normal(0,2,(100, 97))
+w1 = np.random.normal(0,2,(100, 197))
 w2 = np.random.normal(0,2,(10, 101))
 
 
@@ -60,34 +65,35 @@ w2 = np.random.normal(0,2,(10, 101))
 
 
 for e in range(epochs):
-    ee = 0 # errorx
-    for i in range(8):
+    ee = 0 # error
+    for i in range(50):
         # layer 1
         v1 = np.dot(x[i, :], np.transpose(w1))
         y1 = tanh(v1)
         # layer 2
         v2 = np.dot(np.append(y1,1), np.transpose(w2))
         y2 = tanh(v2)
-        #layer 3
-        y3 = np.dot(np.append(y2,1), np.transpose(w3))
         #backprop 
-        err = -np.array(y[i, :]-y3)
-        dEdW3 = np.dot(np.transpose(np.array([err])),np.array([np.append(y2,1)])) # e/dw3
-        errw3 = np.array(np.dot(err, w3))[0:(w3.shape[1] - 1)] # exclude bias since its not part of de/dy2
-        tanhv2errw3 = errw3 * tanh(y2, True)
-        dEdW2 = np.dot(np.transpose(np.array([tanhv2errw3])),np.array([np.append(y1,1)]))# e/dw2
-        tanhv2errw3w2 = np.dot(tanhv2errw3, w2)[0:(w2.shape[1] - 1)]
-        tanhv2errw3w2tanhv1 = tanhv2errw3w2 * tanh(y1, True)
-        dEdW1 = np.dot(np.transpose(np.array([tanhv2errw3w2tanhv1])), np.array([x[i, :]]))# e/dw1
-        ee = ee + ((1.0/2.0) * np.power((y[i, :] - y3), 2.0))
+        err = -np.array(y[i, :]-y2)
+        errphiprimev2 = err*tanh(y2,derive=True)
+
+        dEdW2 = np.dot(np.transpose(np.array([errphiprimev2])), np.array([np.append(y1,1)])) # e/dw2
+
+        errphiprimev2 = np.array(np.dot(errphiprimev2, w2))[0:(w2.shape[1] - 1)] # exclude bias since its not part of de/dy2
+        errphiprimev2w2phiprimev1 = errphiprimev2 * tanh(y1, True)
+        dEdW1 = np.dot(np.transpose(np.array([errphiprimev2w2phiprimev1])), np.array([x[i, :]]))
+ 
+
+        ee = ee + ((1.0/2.0) * np.power((y[i, :] - y2), 2.0))
+
         # adjustments
-        w3 = w3 - eta*dEdW3
+
         w2 = w2 - eta*dEdW2
         w1 = w1 - eta*dEdW1
-        print(ee)
-
+        # print(ee)
+ 
 print('w1----',w1)
 print('w2----',w2)
-print('w3----', w3)
+print(ee)
 
 
